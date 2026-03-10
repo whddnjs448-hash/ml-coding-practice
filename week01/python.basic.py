5822b29f6d221783c9cbfbc40fa915b222217a36
whddnjs448-hash <whddnjs448@gmail.com>
2026-03-10 11:17:39 +0900

    remove venv

--- week01/python.basic.py
+++ week01/python.basic.py
@@ -1,2 +1,79 @@
 a =3 
 b = 4 
+
+#지수 승
+print(a ** b)
+print(a ** 3)
+
+#나머지 연산
+print(a % b)
+print(7 % 3)
+
+#나눗셈 몫 구하기
+print(a // b)
+print(7 //3)
+
+s1 = 'Hello Python'
+print(s1)
+
+s3 = '''Hello
+Python'''
+print(s3)
+
+head = "Python"
+tail = "is fun"
+print(head +tail)
+
+#문자열 곱하기
+print(head *2)
+print("=" * 5)
+
+#문자열 인데싱
+a = "Now is better than never"
+print(a[0])
+print(a[4])
+print(a[-1])
+print(a[-2])
+
+#문자열 슬라이딩
+b = a[0] + a[1] + a[2]
+print(b)
+
+print(a[4:6])
+print(a[19:])
+print(a[:3])
+print(a[7:-11])
+
+#문자 개수 계산 
+a = "Python"
+print(a.count('p'))
+
+#문자 위치 확인
+print(a.find('y'))
+print(a.find('p'))
+print(a.index('y'))
+# print(a.index('p')) #오류 발생
+
+#문자 삽입 
+b = ","
+c = b.join('Abcd')
+print(c)
+
+#대소문자 변환
+print(a.upper())
+print(a.lower())
+
+#공백 제거
+d = "       Py        "
+print(d.lstrip())
+print(d.rstrip())
+print(d.strip())
+
+#문자열 수정(불가능)
+a = "Pithon"
+#a[1] = 'y' # 오류 발생
+
+#문자열 바꾸기 
+a = "Python is diffcult"
+print(a.replace("difficult", "funny"))
+print(a)
