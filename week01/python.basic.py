8e6dc1fd116899d5b733fd83c933e5ffd6ad5ff3
whddnjs448-hash <whddnjs448@gmail.com>
2026-03-10 12:45:26 +0900

    Updated Changes

--- week01/python.basic.py
+++ week01/python.basic.py
@@ -89,4 +89,9 @@ print(b.split(','))
 a= [1,2,3]
 b= ['Life','is', 'too', 'short']
 c = [1,2, 'Life', 'is']
-d= [1,2, [3,4],['']]
+d= [1,2, [3,4],['Life','is']]
+
+#리스트 인덱싱
+print(d[0])
+print(d[2])
+print(d[3][-])
