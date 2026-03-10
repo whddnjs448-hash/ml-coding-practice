f785de3a1974988eae6d20a50e6082ebc4e6a55c
whddnjs448-hash <whddnjs448@gmail.com>
2026-03-10 13:02:55 +0900

    Updated Changes

--- week01/python.basic.py
+++ week01/python.basic.py
@@ -237,4 +237,11 @@ print(s2.union(s3))
 print(s2 - s3)
 print(s3 - s2)
 print(s2.difference(s3))
-print(s3.difference)
+print(s3.difference(s2))
+
+#원소 한 개 추가
+s2.add(7)
+print(s2)
+
+#원소 여러 개 추가
+s2.update()
