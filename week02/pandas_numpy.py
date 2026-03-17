468ac8cf74a1a0c00eb1b043ebc9bc0509a51325
whddnjs448-hash <whddnjs448@gmail.com>
2026-03-17 10:08:01 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -15,4 +15,4 @@ a = pd.DataFrame(sample_dic)
 a.info()
 
 pd.DataFrame([[1,2,],[3,4],[5,6],[7,8]])
-pd.DataFrame([[1,2],[3,4],[5,6],[7,8]]), colu
+pd.DataFrame([[1,2],[3,4],[5,6],[7,8]]), columns = ['var_1','var_2'], index=['a']
