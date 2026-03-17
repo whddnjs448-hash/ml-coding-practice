4923187894e6b59cf8ee16549b467f484207b325
whddnjs448-hash <whddnjs448@gmail.com>
2026-03-17 12:48:19 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -39,4 +39,4 @@ print(sample_df.iloc[0,2])
 print(sample_df.iloc[0:3])
 print(sample_df.loc[0:3, 2:4]) #컬럼까지 동시에 인덱싱
 
-print(sample_df.drop([var_1]))
+print(sample_df.drop(['var_1','var_3'], axis =1)) #컬럼을 제거하려면 a
