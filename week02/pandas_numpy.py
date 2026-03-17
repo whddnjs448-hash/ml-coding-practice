f84bf4c35395fe0df8b7841c513067b59f8f484c
whddnjs448-hash <whddnjs448@gmail.com>
2026-03-17 12:43:49 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -27,3 +27,6 @@ print(sample_df['var_5'])
 
 #print(sample_df['var_1', 'var_2']) #[] 안에는 하나의 값만 들어갈 수 있음
 print(sample_df[['var_1','var_4']]) #[ [] ]를 사용하면 [] 가 하나의 값으로 인식됨
+
+#loc 는 location의 앞글자
+print(sa)
