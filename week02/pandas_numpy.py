f294c3ea4c5281c0ab24c9d35ed11ed1d4f6c773
whddnjs448-hash <whddnjs448@gmail.com>
2026-03-18 12:41:44 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -117,5 +117,6 @@ print(iris['class'].value_counts())
 #예제 데이터 생성
 data = {
     'name': ['Alice', 'Bob', 'Charlie', 'David', 'Eve'],
-    'age': [25, 30, 35, 28, ]
+    'age': [25, 30, 35, 28, 40],
+    'salary': [70000.00, 80000.00, 90000]
 }
