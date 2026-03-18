f512eafe2a60abc0510c313623894f8bd8ef8a56
whddnjs448-hash <whddnjs448@gmail.com>
2026-03-18 12:22:44 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -65,5 +65,5 @@ more2015_or_tv = netflix[(netflix['release_year']> 2015)) | (netflix['type'] == 
 print(more2015_or_tv.head())
 
 data = {
-    'name' : ['Alice', 'Bob', 'Char']
+    'name' : ['Alice', 'Bob', 'Charlie', 'David', 'Eve', 'Frank', 'Grace', 'Han']
 }
