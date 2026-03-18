f93d8f551c31e71e6cd799d2373f119a76522ec2
whddnjs448-hash <whddnjs448@gmail.com>
2026-03-18 12:39:14 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -106,4 +106,5 @@ print(iris.groupby('class').std())
 
 print(iris.drop('class', axis=1)).agg(['sum','mean', 'std'])
 
-#print(iris.agg(['sum','mean', 'std']))
+#print(iris.agg(['sum','mean', 'std'])) #class 컬럼으로 인해 에러 발생
+print(iris['class'].unique())
