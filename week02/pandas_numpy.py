1b32566d13740c94d315e552b1371e7ea2f2ba10
whddnjs448-hash <whddnjs448@gmail.com>
2026-03-18 12:37:14 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -97,4 +97,7 @@ print(sample_df.describe())
 print(sample_df.std())
 print(sample_df.agg(['count', 'mean', 'std', 'min', 'max']))
 
-file_url =
+file_url ='https://media.githubusercontent.com/media/musthave-ML10/data_source/main/iris.csv'
+iris = pd.read_csv(file_url)
+
+print(iris.h)
