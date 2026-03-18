642d6e1dba80c591a17cd7288f1f51f16f9e5234
whddnjs448-hash <whddnjs448@gmail.com>
2026-03-18 12:25:44 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -69,5 +69,5 @@ data = {
     'comment_length': [150, 200,50, 300,120,180,75, 160],
     'likes': [25, 30, 10, 45, 20, 35, 5, 28],
     'is_spam': [False, False, True, False, False, True, False, False],
-    
+    'has_image': [True, False, True, True]
 }
