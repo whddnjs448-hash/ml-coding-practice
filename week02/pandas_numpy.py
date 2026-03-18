95747eec0034b6647e6b44eadce914ab3f437865
whddnjs448-hash <whddnjs448@gmail.com>
2026-03-18 13:04:44 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -179,3 +179,7 @@ print(pd.DataFrame(sample_np, columns = sample_df.columns)) #새 프레임워크
 print(sample_np)
 print(sample_np[0])
 print(sample_np[0,2])
+print(sample_np[0:3,2:4])
+print(sample_np[:,2])
+
+np_a = np.array
