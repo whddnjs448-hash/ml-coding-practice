9fcfc0b1a27365e687c949b577eef7985e4fba0f
whddnjs448-hash <whddnjs448@gmail.com>
2026-03-18 12:24:14 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -67,5 +67,6 @@ print(more2015_or_tv.head())
 data = {
     'name' : ['Alice', 'Bob', 'Charlie', 'David', 'Eve', 'Frank', 'Grace', 'Hannah'],
     'comment_length': [150, 200,50, 300,120,180,75, 160],
-    'likes': [2]
+    'likes': [25, 30, 10, 45, 20, 35, 5, 28],
+    'is_spam'
 }
