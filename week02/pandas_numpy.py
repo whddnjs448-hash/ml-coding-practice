72480c80a3e1f587915f87ff8cd0f991f701c036
whddnjs448-hash <whddnjs448@gmail.com>
2026-03-18 12:20:44 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -61,4 +61,4 @@ print((netflix['release_year']>2015) & (netflix['type'] == 'TV Show'))
 more2015_tv = netflix[(netflix['release_year']> 2015)] & (netflix['type']) == 'TV Show'
 print(more2015_tv.head())
 
-more2015_or
+more2015_or_tv = netflix[(netflix['release_year']> 2015))] | (netflix['type'])
