eccb47553b8c89a17ac3594851123b6945e8cba2
whddnjs448-hash <whddnjs448@gmail.com>
2026-03-18 12:29:44 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -77,6 +77,6 @@ print(df.head())
 
 #필터링 조건 설정
 condition = (
-    (df['comment_lenght'] >=100) &
-    (df['likes']])
+    (df['comment_lenght'] >=100) & #댓글 길이 100자 이상
+    (df['likes'] >= 20) &            
 )
