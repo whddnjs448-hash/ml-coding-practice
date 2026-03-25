559fa345e0be804d0ffe5aeb2f7254ff1e97ef7c
whddnjs448-hash <whddnjs448@gmail.com>
2026-03-25 21:02:44 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -95,4 +95,5 @@ scatter = plt.scatter(x='Age', y='Fare', data=titanic, c=titanic['Survived'],cma
 plt.title('Age and Fare Relationship with Survival on the Titanic')
 plt.xlabel('Age')
 plt.ylabel('Fare')
-plt.legend(handles=scatter.legend_elements())
+plt.legend(handles=scatter.legend_elements()[0],title='Survived',
+           labels =)
