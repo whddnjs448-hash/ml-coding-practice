a8da372f044cf8d91672c4b32089a4efa30ca72d
whddnjs448-hash <whddnjs448@gmail.com>
2026-03-25 13:25:16 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -71,4 +71,6 @@ plt.ylabel('Gender')
 plt.legend(bars, ['Survived - Female', 'Survived - Male'], loc='upper right')
 
 # 차이 강조를 위해 수평선 추가
-plt.axvline(x=survived_counts['male'], color='gray', linestyl)
+plt.axvline(x=survived_counts['male'], color='gray', linestyle='--', linewidth=1)
+
+#생존
