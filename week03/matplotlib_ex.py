9f1eb2782a30ce04e1ea3c7f199389a3dda3d10f
whddnjs448-hash <whddnjs448@gmail.com>
2026-03-25 21:06:14 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -108,4 +108,5 @@ print(survived_counts)
 
 #파이 차트 그리기
 plt.figure(figsize=(8,8))
-plt.pie(survived_counts, labels=['Not Survived', 'Survived'], colors=['orange','gold'])
+plt.pie(survived_counts, labels=['Not Survived', 'Survived'], colors=['orange','gold'],
+        autopct='%0.1f%%', startangle=)
