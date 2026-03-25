eb5d168586ea95f8dd654b08c509303b16d9bea2
whddnjs448-hash <whddnjs448@gmail.com>
2026-03-25 10:18:14 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -23,5 +23,5 @@ pclass_survived_mean
 import matplotlib.pyplot as plt
 
 #선 그래프 그리기
-plt.plot(pclass_survived_mean['Pclass']),pclass_survived_mean['Survived'],
-        marker = ''
+plt.plot(pclass_survived_mean['Pclass'],pclass_survived_mean['Survived'],
+        marker = 'o)
