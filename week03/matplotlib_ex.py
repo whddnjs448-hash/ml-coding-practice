c3df3c1d28f4329252ea7c1e69ca7e7082ab52bd
whddnjs448-hash <whddnjs448@gmail.com>
2026-03-25 11:02:45 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -43,3 +43,4 @@ print(survived_counts)
 #막대 그래프 그리기
 plt.bar(survived_counts.index, survived_counts,
         color = ['mediumorchid', 'darkviolet', 'indigo'])
+plt.title('Survived Counts by Embarked')
