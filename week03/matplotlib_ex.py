936a9bc8574e2fc7f0360323e1da8c67144ebabd
whddnjs448-hash <whddnjs448@gmail.com>
2026-03-25 21:00:44 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -92,4 +92,4 @@ print(titanic.info())
 plt.figure(figsize=(12,8))
 scatter = plt.scatter(x='Age', y='Fare', data=titanic, c=titanic['Survived'],cmap='Set2',alpha=0.7)
 
-plt.title('Age and ')
+plt.title('Age and Fare Relationship with Survival on the Titanic')
