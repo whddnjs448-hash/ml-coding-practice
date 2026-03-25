77b1de3af97fe60d074a3f90e5771d2ac9c2563f
whddnjs448-hash <whddnjs448@gmail.com>
2026-03-25 10:19:44 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -25,4 +25,5 @@ import matplotlib.pyplot as plt
 #선 그래프 그리기
 plt.plot(pclass_survived_mean['Pclass'],pclass_survived_mean['Survived'],
         marker = 'o', linestye='-', color = 'violet')
-plt.title('Survival Rat')
+plt.title('Survival Rate Variation Across Passenger Classes')
+plt.title
