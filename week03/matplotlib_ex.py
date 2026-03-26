b5ccafb0576e9dc02488f05ad6e588b90d97bf29
whddnjs448-hash <whddnjs448@gmail.com>
2026-03-26 14:51:33 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -215,4 +215,4 @@ print(titanic.info())
 plt.figure(figsize=(10,6))
 
 #showmeans=False는 평균값을 표시하지 않도록 하고, showmedians=True는 중앙값을 표시하도록 함
-violin_
+violin_plot = plt.violinplot([titanic[titanic['Pclass']]])
