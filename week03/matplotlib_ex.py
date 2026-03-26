eec69167edc930530b43958026cbe87619e5dd55
whddnjs448-hash <whddnjs448@gmail.com>
2026-03-26 14:18:33 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -164,4 +164,4 @@ titanic = titanic.dropna(subset=['Age','Fare'])
 age_groups = pd.cut(titanic['Age'], bins=range(0, 81, 5))
 
 # Age, Survived 기준으로 그룹화
-survived_counts = titanic.groupby([age_groups, 'Su'])
+survived_counts = titanic.groupby([age_groups, 'Survived'], observed=False).size()
