22179c53f2866c87484c927351eccf20abc6672b
whddnjs448-hash <whddnjs448@gmail.com>
2026-03-26 14:20:33 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -171,4 +171,5 @@ print(survived_counts)
 plt.figure(figsize=(10,6))
 
 #나이대별 생존자 
-plt.fill_between(survived_counts.index.astype(str))
+plt.fill_between(survived_counts.index.astype(str), survived_counts[0],
+                 color=)
