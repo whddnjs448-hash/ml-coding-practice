271fe386c5a315f2958beaf99240b79eca6f55e7
whddnjs448-hash <whddnjs448@gmail.com>
2026-03-26 23:01:27 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -252,4 +252,7 @@ plt.errorbar(fare_means.index, fare_means, yerr=fare_std, fmt='o',
              capsize=5, capthick=1, label='Fare')
 
 plt.title('Error Bar Plot of Fare by Parch')
+plt.xlabel('Parch')
+plt.ylabel('Fare')
+plt.xticks(fare_means.index)
 plt.
