3c849f4a9de9efa9d1764ec0ba8c5b816d17e441
whddnjs448-hash <whddnjs448@gmail.com>
2026-03-26 23:17:27 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -279,4 +279,7 @@ plt.close()
 titanic = pd.read_csv('3.1.1.titanic.csv')
 
 #각 부모와 자녀의 수에 따른 생존자와 사망자 수 계산
-parch_counts = titanic.groupby('Parch')['Survived'].v
+parch_counts = titanic.groupby('Parch')['Survived'].value_counts().unstack().fillna(0)
+print(parch_counts)
+
+#x,y
