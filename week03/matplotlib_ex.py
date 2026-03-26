edd14bece979f25870581fa972324f3dace5a3b8
whddnjs448-hash <whddnjs448@gmail.com>
2026-03-26 14:32:03 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -172,8 +172,8 @@ plt.figure(figsize=(10,6))
 
 #나이대별 생존자 
 plt.fill_between(survived_counts.index.astype(str), survived_counts[1],
-                 color='hotpink',alpha=0.6, label='Not Survived')
+                 color='purple',alpha=0.9, label='Survived')
 
 #나이대별 사망자
 plt.fill_between(survived_counts.index.astype(str), survived_counts[0],
-                 color='hotpink')
+                 color='hotpink', alp)
