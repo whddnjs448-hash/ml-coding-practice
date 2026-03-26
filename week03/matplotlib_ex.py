ec44798253d25520d01a9276ed71a2e28550d324
whddnjs448-hash <whddnjs448@gmail.com>
2026-03-26 14:19:33 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -168,4 +168,7 @@ survived_counts = titanic.groupby([age_groups, 'Survived'], observed=False).size
 print(survived_counts)
 
 #영역 채우기 그래프 그리기
-plt.
+plt.figure(figsize=(10,6))
+
+#나이대별 생존자 
+plt.fill_between(sur)
