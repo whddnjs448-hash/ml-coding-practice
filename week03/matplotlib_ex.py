46ac78e789307a59496aedbcbbc443c49f421981
whddnjs448-hash <whddnjs448@gmail.com>
2026-03-26 23:20:27 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -293,4 +293,5 @@ plt.figure(figsize=(10, 10))
 #첫 번째 서브플롯 설정(선 그래프)
 plt.subplot(2, 1, 1)
 #선 그래프 그리기
-plt.plot(x, y1, '-o', color='indigo', markersize)
+plt.plot(x, y1, '-o', color='indigo', markersize=7, linewidth=3, alpha=0.7,
+         label='Not')
