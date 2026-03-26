06a91918a6a686bb18e42526e329711c6169f884
whddnjs448-hash <whddnjs448@gmail.com>
2026-03-26 14:42:33 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -195,4 +195,5 @@ print(titanic.info())
 
 #승객 등급에 따른 나이의 박스 플롯
 plt.boxplot([titanic[titanic['Pclass'] == 1]['Age'],
-             titanic])
+             titanic[titanic['Pclass'] ==2]['Age'],
+             ])
