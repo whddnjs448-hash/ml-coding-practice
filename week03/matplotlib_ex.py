24579c735136a6fce1bcb925c83cc24df542ec4f
whddnjs448-hash <whddnjs448@gmail.com>
2026-03-26 23:18:27 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -284,4 +284,7 @@ print(parch_counts)
 
 #x,y 설정
 x = parch_counts.index.astype(str) # 데이터프레임의 인덱스를 문자열로 반환
-y1 = parch_counts[0]
+y1 = parch_counts[0].values # 사망자
+y2 = parch_counts[1].values # 생존자
+
+plt.figure(fig)
