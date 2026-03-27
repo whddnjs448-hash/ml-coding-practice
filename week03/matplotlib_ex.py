166ea7a08d9603760c8e1b29162c79a942c79f51
whddnjs448-hash <whddnjs448@gmail.com>
2026-03-27 19:21:35 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -355,4 +355,4 @@ axes[1].tick_params(axis='y', labelcolor='deeppink')
 axes[1].legend(loc='upper right')
 
 # 제목 설정
-fig.suptitle('Survival')
+fig.suptitle('Survival Analysis by Number of Parents/Children ()')
