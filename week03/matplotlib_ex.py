2c1a6263075db048991aa76d4a6acf689ce87768
whddnjs448-hash <whddnjs448@gmail.com>
2026-03-27 19:14:35 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -338,4 +338,7 @@ y1 = parch_counts[0].values # 사망자
 y2 = parch_counts[1].values # 생존자
 
 # plt.subplots() 함수를 사용하여 하나의 그림인 figure에 개별 서브플롯을 동시에 추가
-fig, axes = plt.subplots(2, 1, figsize=)
+fig, axes = plt.subplots(2, 1, figsize=(10, 10))
+
+# 첫 번쨰 서브플롯 설정(선 그래프)
+axes[0].plot(x, y1, )
