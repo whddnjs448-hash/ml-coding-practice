90120a7e8107ea56bf02218d35faf23321c4042d
whddnjs448-hash <whddnjs448@gmail.com>
2026-03-27 19:16:05 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -341,4 +341,6 @@ y2 = parch_counts[1].values # 생존자
 fig, axes = plt.subplots(2, 1, figsize=(10, 10))
 
 # 첫 번쨰 서브플롯 설정(선 그래프)
-axes[0].plot(x, y1, '-o', color='indigo', markersize=7, linewidth=3, alpha=0.7, label= 'Not Surv')
+axes[0].plot(x, y1, '-o', color='indigo', markersize=7, linewidth=3, alpha=0.7, label= 'Not Survived')
+axes[0].set_xlabel('Parch')
+axes[0].set_
