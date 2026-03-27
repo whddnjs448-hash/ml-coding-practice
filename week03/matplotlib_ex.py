1ddca67afc7d9b4aad7c244af16b5de14aca075e
whddnjs448-hash <whddnjs448@gmail.com>
2026-03-27 19:44:33 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -386,4 +386,7 @@ ax1.set_ylabel('Not Survived Count', color='indigo') # y축의 레이블
 ax1.tick_params(axis='y', labelcolor='indigo') # 눈금의 모양과 속성
 ax1.legend(loc='upper right', bbox_to_anchor=(1, 1)) # 범례
 
-# .twinx()
+# .twinx() 함수를 사용하여 두 번째 그래프와 축을 공유하는 새로운 Axes 객체 생성
+ax2 = ax1.twinx()
+
+# 두 번째
