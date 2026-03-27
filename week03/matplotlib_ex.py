c77de2a7fe5dbf651773370f39c8cb8353b5fb46
whddnjs448-hash <whddnjs448@gmail.com>
2026-03-27 19:00:35 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -306,3 +306,6 @@ plt.xlabel('Parch')
 plt.ylabel('Survived Count', color='deeppink')
 plt.tick_params(axis='y', labelcolor='deeppink')
 plt.legend(loc='upper right')
+
+# 제목 설정
+plt.suptitle('Sur')
