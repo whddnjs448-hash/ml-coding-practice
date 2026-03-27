150446f5ed1e52649d4092ffc15f1c4cb9195c39
whddnjs448-hash <whddnjs448@gmail.com>
2026-03-27 19:12:35 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -333,4 +333,5 @@ parch_counts = titanic.groupby('Parch')['Survived'].value_counts().unstack().fil
 print(parch_counts)
 
 # x,y값 설정
-x = parch_counts.index
+x = parch_counts.index.astype(str) # 데이터프레임의 인덱스를 문자열로 변환
+y1 = parch_counts[0].values #사
