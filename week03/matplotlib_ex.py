a507fe69469968f584116e3c27fd4a5acf5aea34
whddnjs448-hash <whddnjs448@gmail.com>
2026-03-27 19:43:33 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -383,4 +383,5 @@ fig, ax1 = plt.subplots()
 ax1.plot(x, y1, '-s', color='indigo', markesize=7, linewidth=5, alpha=0.7, label='Not Survived')
 ax1.set_xlabel('Parch') # x축의 레이블
 ax1.set_ylabel('Not Survived Count', color='indigo') # y축의 레이블
-ax1.tick_params(axis='y', labelcolor)
+ax1.tick_params(axis='y', labelcolor='indigo') # 눈금의 모양과 속성
+ax1.legend(loc='upper right')
