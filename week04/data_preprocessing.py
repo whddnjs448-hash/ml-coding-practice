ae7ff94e1c630b7d18c76c6449a54583261a9d9e
whddnjs448-hash <whddnjs448@gmail.com>
2026-03-30 16:43:02 +0900

    Updated Changes

--- week04/data_preprocessing.py
+++ week04/data_preprocessing.py
@@ -38,4 +38,7 @@ for set_ in (strat_test_set, strat_test_set):
 * 'inplace=True'로 지정하지 않은 한 'strat_train_set' 자체를 수정하지 않음
 """
 housing = strat_test_set.drop("median_house_value", axis=1)
-housing_labels = st
+housing_labels = strat_test_set["median_house_value"].copy()
+
+# 데이터 정제
+# nul
