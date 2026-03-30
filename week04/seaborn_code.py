f8b0731c00f0051a5f414155ec29899faf98276b
whddnjs448-hash <whddnjs448@gmail.com>
2026-03-30 15:47:01 +0900

    Updated Changes

--- week04/seaborn_code.py
+++ week04/seaborn_code.py
@@ -68,4 +68,5 @@ plt.savefig('./week04/Seaborn_Figure03.jpg')
 # 히스토그램과 커널 밀도 추정 그래프 함께 그리기
 sns.histplot(tips['tip'], bins=30, kde=True, color='skyblue')
 
-plt.title('Hist')
+plt.title('Histogram with KDE for Tips')
+plt.savefig('./week04/Seaborn')
