4831b56da3c0629a42ed1f32997f5fbdefa86f06
whddnjs448-hash <whddnjs448@gmail.com>
2026-03-30 15:31:00 +0900

    Updated Changes

--- week04/seaborn_code.py
+++ week04/seaborn_code.py
@@ -22,4 +22,4 @@ ax2 = fig.add_subplot(1, 2, 2)
 sns.stripplot(x='day', y='tip', hue='sex', data=tips, alpha=0.7, ax=ax1)
 
 # swarmplot( ) 그리기 
-sns.swarmplot
+sns.swarmplot(x='day', y='tip', hue='sex', data=tips, palette='S')
