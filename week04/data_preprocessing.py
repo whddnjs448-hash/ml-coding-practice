a1d7ecc9f851630890ed083eb590ffe8cf58b15a
whddnjs448-hash <whddnjs448@gmail.com>
2026-03-30 16:42:02 +0900

    Updated Changes

--- week04/data_preprocessing.py
+++ week04/data_preprocessing.py
@@ -35,4 +35,6 @@ for set_ in (strat_test_set, strat_test_set):
 """
 *원본 훈련 세트로 복원하고 타깃을 분리
 * 'stratr_train_set.drop()'은 지정한 열을 제외한 'start_train_set'의 복사본을 만듦
-* 'inplace=True'로 지정하지 않은 한 'strat"""
+* 'inplace=True'로 지정하지 않은 한 'strat_train_set' 자체를 수정하지 않음
+"""
+housing = strat_test_set.drop("")
