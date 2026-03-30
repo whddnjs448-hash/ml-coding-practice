ef4f67cd8c7f639a1eab6a2d569e50211e70366a
whddnjs448-hash <whddnjs448@gmail.com>
2026-03-30 15:48:31 +0900

    Updated Changes

--- week04/seaborn_code.py
+++ week04/seaborn_code.py
@@ -74,3 +74,6 @@ plt.savefig('./week04/Seaborn_Figure04.jpg')
 # **조인트 그래프**
 # jointplot( ) 그리기
 sns.jointplot(x='size', y='tip', data=tips, kind='scatter')
+plt.savefig('./week04/Seaborn_Figure05.jpg')
+
+# *
