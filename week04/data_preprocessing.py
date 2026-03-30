567491197518a738406bf59a98d46bcef9a50bd9
whddnjs448-hash <whddnjs448@gmail.com>
2026-03-30 16:38:02 +0900

    Updated Changes

--- week04/data_preprocessing.py
+++ week04/data_preprocessing.py
@@ -23,4 +23,6 @@ housing = load_housing_data()
 from sklearn.model_selection import train_test_split
 
 housing["income_cat"] = pd.cut(housing["median_income"],
-                               bins=[0., ])
+                               bins=[0., 1.5, 3.0, 4.5, 6., np.inf],
+                               labels=[1, 2, 3, 4, 5])
+
