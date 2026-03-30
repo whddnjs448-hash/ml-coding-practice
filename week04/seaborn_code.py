5c23354a59c1ae35f179ac18a84a53c8d6c7caac
whddnjs448-hash <whddnjs448@gmail.com>
2026-03-30 15:46:01 +0900

    Updated Changes

--- week04/seaborn_code.py
+++ week04/seaborn_code.py
@@ -64,4 +64,6 @@ ax1.set_title('fit_reg = True')
 ax2.set_title('fit_reg = False')
 plt.savefig('./week04/Seaborn_Figure03.jpg')
 
-# **히스토그램과 커널 밀
+# **히스토그램과 커널 밀도 추정 그래프**
+# 히스토그램과 커널 밀도 추정 그래프 함께 그리기
+sns.histplot(tips['tip'], bins)
