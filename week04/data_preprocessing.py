d4e09468a443e35847aa8be848de3e4fc72541b2
whddnjs448-hash <whddnjs448@gmail.com>
2026-03-30 16:39:01 +0900

    Updated Changes

--- week04/data_preprocessing.py
+++ week04/data_preprocessing.py
@@ -26,4 +26,4 @@ housing["income_cat"] = pd.cut(housing["median_income"],
                                bins=[0., 1.5, 3.0, 4.5, 6., np.inf],
                                labels=[1, 2, 3, 4, 5])
 
-strat_train_set, 
+strat_train_set, strat_test_set = trai
