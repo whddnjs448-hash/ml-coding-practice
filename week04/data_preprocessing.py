560aa2ad8188a2bbc3481d62de7084eca4922335
whddnjs448-hash <whddnjs448@gmail.com>
2026-03-30 16:36:01 +0900

    Updated Changes

--- week04/data_preprocessing.py
+++ week04/data_preprocessing.py
@@ -13,4 +13,6 @@ def load_housing_data():
         Path("datasets").mkdir(parents=True, exist_ok=True)
         url = "https://github.com/ageron/data/raw/main/housing.tgz"
         urllib.request.urlretrieve(url, tarball_path)
-        with tarfile.open(tarball_path) as housing_tar
+        with tarfile.open(tarball_path) as housing_tarball:
+            housing_tarball.extractall(path="datasets")
+    retrun pd.read_csv
