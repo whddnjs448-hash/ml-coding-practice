ee36b14ae0a8db6d6c565ed75cc6c038e66e32ac
whddnjs448-hash <whddnjs448@gmail.com>
2026-03-30 17:00:02 +0900

    Updated Changes

--- week04/data_preprocessing.py
+++ week04/data_preprocessing.py
@@ -86,4 +86,8 @@ housing_cat.head(8)
 from sklearn.preprocessing import OrdinalEncoder
 
 ordinal_encoder = OrdinalEncoder()
-housing_cat_encoded = ordinal_encoder.fi
+housing_cat_encoded = ordinal_encoder.fit_transform(housing_cat)
+
+housing_cat_encoded[:8]
+
+ordinal_encoder.categories_
