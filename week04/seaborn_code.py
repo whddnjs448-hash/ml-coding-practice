0de6a6fe840245d599aaf63f36c3da06b4b45034
whddnjs448-hash <whddnjs448@gmail.com>
2026-03-30 15:45:01 +0900

    Updated Changes

--- week04/seaborn_code.py
+++ week04/seaborn_code.py
@@ -60,4 +60,6 @@ sns.regplot(x='total_bill', y='tip', data=tips, color='blue', scatter_kws={'s': 
 sns.regplot(x='total_bill', y='tip', data=tips, color='blue', scatter_kws={'s': 50, 'alpha': 0.5}, line_kws={'linestyle' : '--'}, ax=ax2, fit_reg=False)
 
 fig.suptitle('Scatter Plots with Regression Lines', fontsize=16)
-ax1.set_title('fit_reg = ')
+ax1.set_title('fit_reg = True')
+ax2.set_title('fit_reg = False')
+plt.savefig('./w')
