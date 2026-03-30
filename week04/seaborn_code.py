da87fa0b0e38e5bbbd05736cc4cfb0e77b640767
whddnjs448-hash <whddnjs448@gmail.com>
2026-03-30 15:35:01 +0900

    Updated Changes

--- week04/seaborn_code.py
+++ week04/seaborn_code.py
@@ -37,4 +37,7 @@ ax2 = fig.add_subplot(1, 2, 2)
 
 #식사가 이루어진 시간대 파악
 # x축 변수, 데이터셋, axe 객체(1번째 그래프)
-sns.countplot(x='time' , data)
+sns.countplot(x='time' , data=tips, ax=ax1)
+
+# 식사가 이루어진 시간대 파악과 식사가 이루어진 요일로 색상 분류
+# x축 변수, hue로 생강 부
