11174ab18c5b5f0322e678edadb7c6fc42051629
whddnjs448-hash <whddnjs448@gmail.com>
2026-04-04 16:30:48 +0900

    Updated Changes

--- week04/data_preprocessing-2.py
+++ week04/data_preprocessing-2.py
@@ -30,3 +30,7 @@ housing_labels = strat_train_set["median_house_value"].copy()
 
 # 데이터 정제
 # null 값이 있는 행 확인하기
+null_rows_idx = housing.isnull().any(axis=1)
+housing.loc[null_rows_idx].head()
+
+from sklearn.impute import SimpleImputer
