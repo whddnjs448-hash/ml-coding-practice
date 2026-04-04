17f9ab18ae71d2fd1d5996e028677db959051367
whddnjs448-hash <whddnjs448@gmail.com>
2026-04-04 16:31:48 +0900

    Updated Changes

--- week04/data_preprocessing-2.py
+++ week04/data_preprocessing-2.py
@@ -36,3 +36,8 @@ housing.loc[null_rows_idx].head()
 from sklearn.impute import SimpleImputer
 
 imputer = SimpleImputer(strategy="median")
+
+# 수치형 특성만 추출
+housing_num = housing.select_dtypes(include=[np.number])
+housing_num.head()
+
