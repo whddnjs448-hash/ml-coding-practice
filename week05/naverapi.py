8d99bf834348cfbed8d48de9d8c2bfbe75547e3d
whddnjs448-hash <whddnjs448@gmail.com>
2026-04-16 15:42:45 +0900

    Updated Changes

--- week05/naverapi.py
+++ week05/naverapi.py
@@ -27,4 +27,5 @@ def main() :
         
     print('전체 검색 : %d 건' %total)
     
-    with open('%s_naver_%s.json' % ())
+    with open('%s_naver_%s.json' % (srcText, node), 'w', encoding= 'utf8') as outfile:
+        
