d603e98c2de9a10ccb483e993782e531db947df7
whddnjs448-hash <whddnjs448@gmail.com>
2026-04-16 15:48:45 +0900

    Updated Changes

--- week05/naverapi.py
+++ week05/naverapi.py
@@ -40,4 +40,4 @@ def main() :
 def getNaverSearch(node, srcText, page_start, display):
     base = "https://openapi.naver.com/v1/search"
     node = "/%s.json" % node
-    
+    parameters = "?query=%s&start=%s&display=%s" % (urllib.parse.quote(srcText), page_start, display)
