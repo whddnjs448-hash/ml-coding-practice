602353eb73147c50826b56c78accdbd7b9810e4a
whddnjs448-hash <whddnjs448@gmail.com>
2026-04-17 18:56:16 +0900

    Updated Changes

--- week05/tourism.py
+++ week05/tourism.py
@@ -44,14 +44,14 @@ def getTourismStatsService(nat_cd, ed_cd, nStartYear, nEndYear):
         #jsonData를 출려하여 확인........................
          print(json.dumps(jsonData, indent = 4, sort_keys = True, ensure_ascii = False))
         
-        natName = jsonData['response']['body']['items']['item']['natKorNm']
-        natName = natName.replace(' ', '')
-        num = jsonData['response']['body']['items']['item']['num']
-        ed = jsonData['response']['body']['items']['item']['ed']
-        print('[ %s_%s : %s ]' % (natName, yyyymm, num))
-        print('------------------------------------------------------')
-        jsonResult.append({'nat_name': natName, 'nat_cd': nat_cd, 'yyyymm': yyyymm, 'visit_cnt': num})
-        result.append([natName, nat_cd, yyyymm, num])
+         natName = jsonData['response']['body']['items']['item']['natKorNm']
+         natName = natName.replace(' ', '')
+         num = jsonData['response']['body']['items']['item']['num']
+         ed = jsonData['response']['body']['items']['item']['ed']
+         print('[ %s_%s : %s ]' % (natName, yyyymm, num))
+         print('------------------------------------------------------')
+         jsonResult.append({'nat_name': natName, 'nat_cd': nat_cd, 'yyyymm': yyyymm, 'visit_cnt': num})
+         result.append([natName, nat_cd, yyyymm, num])
         
   return (jsonResult, result, natName, ed) 
 
