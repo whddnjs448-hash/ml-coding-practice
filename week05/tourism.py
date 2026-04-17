cd372c806ae6b56133a6eeeacde2ce5d7d030097
whddnjs448-hash <whddnjs448@gmail.com>
2026-04-17 18:46:45 +0900

    Updated Changes

--- week05/tourism.py
+++ week05/tourism.py
@@ -83,5 +83,9 @@ def getRequestUrl(url):  #[CODE 1]
       print("[%s] Url Request Success" % datetime.datetime.now())
       return response.read().decode('utf-8')
   except Exception as e:
-    
+    print(e)
+    print("[%s] Error for URL : %s" % (datetime.datetime.now(), url))
+    return None
+
+main()
     
