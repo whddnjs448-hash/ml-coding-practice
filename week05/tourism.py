b215af89f17060dc62f9d39596bac0e098dadd94
whddnjs448-hash <whddnjs448@gmail.com>
2026-04-17 18:33:15 +0900

    Updated Changes

--- week05/tourism.py
+++ week05/tourism.py
@@ -39,3 +39,5 @@ def getTourismStatsService(nat_cd, ed_cd, nStartYear, nEndYear):
          #데이터가 없는 마지막 항목인 경우 -----------------
          if jsonData['response']['body']['items'] == '':
            dataEND = "{0}{1:0>2}".format(str(year), str(month-1))
+           print("데이터 없음.... \n제공되는 통계 데이터는 %s년 %s월까지입니다." % (str(year), str(month-1)))
+          break
