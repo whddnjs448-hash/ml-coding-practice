d8c705a4ee790a7e05f414900d38e553a5a99197
whddnjs448-hash <whddnjs448@gmail.com>
2026-04-17 18:30:45 +0900

    Updated Changes

--- week05/tourism.py
+++ week05/tourism.py
@@ -32,4 +32,7 @@ def getTourismStatsService(nat_cd, ed_cd, nStartYear, nEndYear):
   result = []
   
   for year in range(nStartYear, nEndYear+1):
-    for mon
+    for month in range(1, 13):
+      yyyymm = "{0}{1:0>2}".format(str(year), str(month))
+      jsonData = getTourismStatsItem(yyyymm, nat_cd, ed_cd)     #[CODE 2]
+      
