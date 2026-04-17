26652642b34766deec15a844b31ec912fb091939
whddnjs448-hash <whddnjs448@gmail.com>
2026-04-17 18:07:45 +0900

    Updated Changes

--- week05/naverapi.py
+++ week05/naverapi.py
@@ -58,4 +58,7 @@ def getRequestUrl(url):
     
     try:
         response = urllib.request.urlopen(req)
-        if response.getcode
+        if response.getcode() == 200:
+            print("[%s] Url Request Success" % datetime.datetime.now())
+            return response.read().decode('utf-8')
+    
