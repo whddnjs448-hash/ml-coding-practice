e2aa99312e9196f9c6d2c9ea98433ea5ce1d08b1
whddnjs448-hash <whddnjs448@gmail.com>
2026-04-17 18:13:44 +0900

    Updated Changes

--- week05/naverapi.py
+++ week05/naverapi.py
@@ -78,5 +78,5 @@ def getPostData(post, jsonResult, cnt):    #[CODE 3]
     # %d: 일 (예, 01, 02, 03, ...)
     # %b: 짧은 형식의 월 이름  (예, 'Jan', 'Feb', 'Mar', ...)
     pDate = datetime.datetime.strptime(post['pubDate'],'%a, %d %b %Y %H:%M:%S +0900')
-    pDate =
+    pDate = pdate.strftime('%Y-%m-%d %H')
     
