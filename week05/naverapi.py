eea1bfc66c5ba06498de8d6e867b89987854cfec
whddnjs448-hash <whddnjs448@gmail.com>
2026-04-17 18:14:45 +0900

    Updated Changes

--- week05/naverapi.py
+++ week05/naverapi.py
@@ -80,4 +80,7 @@ def getPostData(post, jsonResult, cnt):    #[CODE 3]
     pDate = datetime.datetime.strptime(post['pubDate'],'%a, %d %b %Y %H:%M:%S +0900')
     pDate = pdate.strftime('%Y-%m-%d %H:%M:%S')
 
-    jsonResult.append()    
+    jsonResult.append({
+        'cnt' : cnt,
+        ''
+    })    
