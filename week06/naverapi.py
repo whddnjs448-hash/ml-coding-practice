a84374a74fa3a33c277712248fa67997e39a9103
whddnjs448-hash <whddnjs448@gmail.com>
2026-04-20 11:39:15 +0900

    Updated Changes

--- week06/naverapi.py
+++ week06/naverapi.py
@@ -25,4 +25,8 @@ def main():
             start = jsonResponse['start'] + jsonResponse['display']
         jsonResponse = getNaverSearch(node, srcText, start, 100) # [CODE 2]
         
-    print('전체 검색 : %d')
+    print('전체 검색 : %d 건' %total)
+    
+    with open('%s_naver_%s.json' % (srcText, node), 'w', encoding = 'utf8') as outfile:
+        jsonFile = json.dumps(jsonResult, indent = 4, sort_keys = True,
+                              ensure_ascii = False)
