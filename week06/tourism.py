9ea98cadec5031167660fcdf8410e3ff106a5ac8
whddnjs448-hash <whddnjs448@gmail.com>
2026-04-20 12:20:45 +0900

    Updated Changes

--- week06/tourism.py
+++ week06/tourism.py
@@ -57,10 +57,11 @@ def getTourisnStatsService(nat_cd, ed_cd, nStartYear, nEndYear):
 
 """### [CODE 2]"""
 
-def getTourismStatsItem(yyyymm, nat_cd, cd_cd):
+def getTourismStatsItem(yyyymm, nat_cd, ed_cd):
    service_url = "http://openapi.tour.go.kr/openapi/service/EdrcntTourismStatsService/getEdrcntTourismStatsList"
    parameters = "?_type=json&serviceKey=" + ServiceKey   #인증키
    parameters += "&YM=" + yyyymm 
    parameters += "&NAT_CD=" + nat_cd
+   parameters += "&ED_CD=" + ed_cd
    
   
