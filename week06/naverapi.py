5164b858a72d63453025c52f860adda256545485
whddnjs448-hash <whddnjs448@gmail.com>
2026-04-20 11:50:45 +0900

    Updated Changes

--- week06/naverapi.py
+++ week06/naverapi.py
@@ -71,4 +71,10 @@ def getRequestUrl(url):
 def getPostData(post, jsonResult, cnt):   #[CODE 3]
     title = post['title']
     desrciption = post['description']
-    org_link = post['ori']
+    org_link = post['originallink']
+    link = post['link']
+    
+    # %a: 짧은 형식의 요일 이름 (예, 'Mon', "Tue', "Wed', ... )
+    # %d: 일 (예, 01, 02, 03, ...)
+    # %b: 짧은 형식의 월 이름  (예, 'Jan', 'Feb', 'Mar', ...) 
+    
