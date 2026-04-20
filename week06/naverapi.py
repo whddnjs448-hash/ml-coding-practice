621a594d59beae131774d07e72f4f386a4921be9
whddnjs448-hash <whddnjs448@gmail.com>
2026-04-20 11:49:45 +0900

    Updated Changes

--- week06/naverapi.py
+++ week06/naverapi.py
@@ -68,4 +68,6 @@ def getRequestUrl(url):
         return None
     
 
-def getPostData(post,jsonResult, cnt)
+def getPostData(post, jsonResult, cnt):   #[CODE 3]
+    title = post['title']
+    desrcip
