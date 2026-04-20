225c50f066b0d07d62089afb76a9c9e0fada7e89
whddnjs448-hash <whddnjs448@gmail.com>
2026-04-20 12:12:15 +0900

    Updated Changes

--- week06/tourism.py
+++ week06/tourism.py
@@ -42,4 +42,4 @@ def getTourisnStatsService(nat_cd, ed_cd, nStartYear, nEndYear):
           print("데이터 없음.... \n제공되는 통계 데이터는 %s년 %s월까지입니다. " % (str(year), str(month-1)))
           break
         #jsonData를 출력하여 확인............................................
-        print()
+        print(json.dumps(jsonData, indent=4, sort_keys= True, ensure_ascii=))
