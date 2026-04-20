92654a0332caaf98bbd163f4bfe69ed876a757fb
whddnjs448-hash <whddnjs448@gmail.com>
2026-04-20 11:37:45 +0900

    Updated Changes

--- week06/naverapi.py
+++ week06/naverapi.py
@@ -17,4 +17,5 @@ def main():
     jsonResponse = getNaverSearch(node, srcText, 1, 100)      # [CODE 2]
     total = jsonResponse['total']
     
-    while ((jsonResponse != None) and ())
+    while ((jsonResponse != None) and (jsonResponse['display'] != 0)):
+        for post in jsonResponse['items']
