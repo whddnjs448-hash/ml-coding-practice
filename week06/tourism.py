7d43c3d8c7be68653e103965673a547edbee3b77
whddnjs448-hash <whddnjs448@gmail.com>
2026-04-20 13:07:06 +0900

    Updated Changes

--- week06/tourism.py
+++ week06/tourism.py
@@ -64,13 +64,4 @@ def getTourismStatsItem(yyyymm, nat_cd, ed_cd):
    parameters += "&NAT_CD=" + nat_cd
    parameters += "&ED_CD=" + ed_cd
    
-  url = service_url + parameters
-  
-  responseDecode = getRequestUrl(url) 	                    #[CODE 1]
-  
-  if (responseDecode == None):
-    return None
-  else:
-    return json.loads(responseDecode)
-  
-"""### [CODE 1]"""
+   url = service_url + par
