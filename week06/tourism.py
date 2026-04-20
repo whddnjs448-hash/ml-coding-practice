16e7756af57013253d5faabcd5aa9a77586de09e
whddnjs448-hash <whddnjs448@gmail.com>
2026-04-20 12:16:46 +0900

    Updated Changes

--- week06/tourism.py
+++ week06/tourism.py
@@ -51,4 +51,6 @@ def getTourisnStatsService(nat_cd, ed_cd, nStartYear, nEndYear):
         print('[ %s_%s : %s ]' % (natName, yyyymm, num))
         print('------------------------------------------------------')
         jsonResult.append({'nat_name': natName, 'nat_cd': nat_cd, 'yyyymm': yyyymm, 'visit_cnt': num})
-        res
+        result.append([natName, nat_cd, yyyymm, num])
+        
+      
