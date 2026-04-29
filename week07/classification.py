7c68b9b13a67f3a2736888614645800e36bba2c9
whddnjs448-hash <whddnjs448@gmail.com>
2026-04-29 14:50:19 +0900

    Updated Changes

--- week07/classification.py
+++ week07/classification.py
@@ -39,4 +39,4 @@ X_train, X_test, y_train, y_test = X[:60000], X[60000:], y[:60000], y[60000:]
 y_train_5 = (y_train == '5') # 5는 True고, 다른 숫자는 모두 False
 y_test_5 = (y_test == '5')
 
-from sklearn.l
+from sklearn.linear_model
