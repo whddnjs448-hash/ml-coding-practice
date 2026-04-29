615b2e5fa36e805e4f4f3d327104be928d1d832a
whddnjs448-hash <whddnjs448@gmail.com>
2026-04-29 14:51:49 +0900

    Updated Changes

--- week07/classification.py
+++ week07/classification.py
@@ -42,3 +42,7 @@ y_test_5 = (y_test == '5')
 from sklearn.linear_model import SGDClassifier
 
 sgd_clf = SGDClassifier(random_state=42)
+sgd_clf.fit(X_train, y_train_5)
+
+sgd_clf.predict([some_digit])
+
