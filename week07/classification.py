24d2a16685e3fe806387e3778c3ace85f616ca90
whddnjs448-hash <whddnjs448@gmail.com>
2026-04-29 10:09:44 +0900

    다음

--- week07/classification.py
+++ week07/classification.py
@@ -1,2 +1,40 @@
 # -*- coding: utf-8 -*-
 from sklearn.datasets import fetch_openml
+
+mnist = fetch_openml('mnist_784', as_frame=False)
+
+print(mnist.keys())  # data와 target만 사용
+
+X, y = mnist.data, mnist.target
+print(X)
+print(X.shape)       # 28 x 28 개의 픽셀 특징을 가진 이미지 70,000개
+print(y)
+print(y.shape)
+
+import matplotlib.pyplot as plt
+
+def plot_digit(image_data):
+    image = image_data.reshape(28, 28)
+    plt.imshow(image, cmap="binary")
+    plt.axis("off")
+    
+some_digit = X[0]
+plot_digit(some_digit)
+plt.show()
+
+print(y[0])         # 샘플 데이터 레이블 확인 (=5)
+
+# 10x10 그림 생성
+plt.figure(figsize=(9,9))
+for idx, image_data in enumerate(X[:100]):
+    plt.subplot(10, 10, idx + 1)
+    plot_digit(image_data)
+plt.subplots_adjust(wspace=0, hspace=0)
+plt.show()
+
+# train/test 데이터셋 나누기
+X_train, X_test, y_train, y_test = X[:60000], X[60000:], y[:60000], y[60000:]
+
+# 이진 분류기 훈련
+y_train_5 = (y_train == '5')  # 5는 True고, 다른 숫자는 모두 False
+y_test_5 = (y_test == '5')
