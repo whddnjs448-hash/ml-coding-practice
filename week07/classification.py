11e64ff43a6ee22f7d909fe8ce0b567724095a1c
whddnjs448-hash <whddnjs448@gmail.com>
2026-04-29 14:56:50 +0900

    Updated Changes

--- week07/classification.py
+++ week07/classification.py
@@ -75,4 +75,10 @@ print(confusion_matrix(y_train_5, y_train_perfect_predictions))
 # 정밀도와 재현율
 from sklearn.metrics import precision_score, recall_score
 
-p
+print(precision_score(y_train_5, y_train_pred))  # == 3530 / (687 + 3530)
+
+print(recall_score(y_train_5, y_train_pred))  # == 3530 / (1891 + 3530)
+
+from sklearn.metrics import f1_score
+
+print
