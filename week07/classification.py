2835e6eaaf07e3c18368c795366a8c216d25b626
whddnjs448-hash <whddnjs448@gmail.com>
2026-04-29 14:37:48 +0900

    Updated Changes

--- week07/classification.py
+++ week07/classification.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
 from sklearn.datasets import fetch_openml
 
-mnist = fetch_openml('mnist_784', as_frame+)
+mnist = fetch_openml('mnist_784', as_frame=False)
+
+print(mnist.key())   #data와 target만 사용
+
