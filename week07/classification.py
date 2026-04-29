52579bc6fa2c77c890983344a60080c2cd7d21d7
whddnjs448-hash <whddnjs448@gmail.com>
2026-04-29 14:39:19 +0900

    Updated Changes

--- week07/classification.py
+++ week07/classification.py
@@ -5,4 +5,6 @@ mnist = fetch_openml('mnist_784', as_frame=False)
 
 print(mnist.key())   #data와 target만 사용
 
-X, y = mnist.data, mnist.target
+X, y = mnist.data, mnist.target 
+print(X)
+print(X.shape) #28 x 28개의 픽셀 특징을 가진 이미지 70,00개
