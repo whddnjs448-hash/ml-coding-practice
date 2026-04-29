7ac485b619b3245fa6a7d1a9c3b73a1d593ba215
whddnjs448-hash <whddnjs448@gmail.com>
2026-04-29 14:51:20 +0900

    Updated Changes

--- week07/classification.py
+++ week07/classification.py
@@ -39,4 +39,6 @@ X_train, X_test, y_train, y_test = X[:60000], X[60000:], y[:60000], y[60000:]
 y_train_5 = (y_train == '5') # 5는 True고, 다른 숫자는 모두 False
 y_test_5 = (y_test == '5')
 
-from sklearn.linear_model
+from sklearn.linear_model import SGDClassifier
+
+sgd_clf = SGDClassifier(random_state=42)
