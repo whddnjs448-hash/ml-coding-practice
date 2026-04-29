b20d1c63e6383afd8ece79726dd3919b5817a42d
whddnjs448-hash <whddnjs448@gmail.com>
2026-04-29 14:41:49 +0900

    Updated Changes

--- week07/classification.py
+++ week07/classification.py
@@ -15,4 +15,7 @@ import matplotlib.pyplot as plt
 
 def plot_digit(image_data):
     image = image_data.reshape(28, 28)
-    plt.imshow()
+    plt.imshow(image, cmap="binary")
+    plt.axis("off")
+    
+s
