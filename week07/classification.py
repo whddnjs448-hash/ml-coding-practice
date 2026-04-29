4c4f4018e0c810fcd8f038230dcd9dc5c477e512
whddnjs448-hash <whddnjs448@gmail.com>
2026-04-29 14:53:49 +0900

    Updated Changes

--- week07/classification.py
+++ week07/classification.py
@@ -51,4 +51,9 @@ from sklearn.model_selection import cross_val_score
 
 cross_val_score(sgd_clf, X_train, y_train_5, cv=3, scoring="accuracy")
 
-from sklearn.dummy import Dumm
+from sklearn.dummy import DummyClassifier
+
+dummy_clf = DummyClassifier()
+dummy_clf.fit(X_train, y_train_5)
+print(any(dummy_clf.predict(X_train)))
+
