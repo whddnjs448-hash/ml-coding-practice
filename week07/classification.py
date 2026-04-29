a95204a0a400210852886a202f6e8879e2a60f39
whddnjs448-hash <whddnjs448@gmail.com>
2026-04-29 14:56:19 +0900

    Updated Changes

--- week07/classification.py
+++ week07/classification.py
@@ -73,4 +73,6 @@ y_train_perfect_predictions = y_train_5  # 완벽한 분류기일 경우
 print(confusion_matrix(y_train_5, y_train_perfect_predictions))
 
 # 정밀도와 재현율
-from sklearn.metrics import 
+from sklearn.metrics import precision_score, recall_score
+
+p
