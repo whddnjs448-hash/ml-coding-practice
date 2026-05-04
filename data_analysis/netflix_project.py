ab0fe0f9190593282979cd792f8e4ca388c689cd
whddnjs448-hash <whddnjs448@gmail.com>
2026-05-05 00:11:29 +0900

    .

--- data_analysis/netflix_project.py
+++ data_analysis/netflix_project.py
@@ -141,3 +141,10 @@ plt.grid(axis='x')
 plt.show()
 
 netflix[netflix['title'].str.contains('Sankofa', na=False, case=False)]
+
+# 출력할 최대 행 수를 None으로 설정해서 모두 출력
+pd.set_option('display.max_rows', None)
+
+# 쉼표로 country 열의 값을 파이썬 리스트로 만들기
+netflix['country'] = netflix['country'].str.split(', ')
+netflix['country']
