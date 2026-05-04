5bca6a025852beeec8ff8a0c048e0e4d09905033
whddnjs448-hash <whddnjs448@gmail.com>
2026-05-05 02:05:11 +0900

    .

--- data_analysis/netflix_project.py
+++ data_analysis/netflix_project.py
@@ -221,3 +221,7 @@ plt.suptitle('Keywords in the description of Movies and TV shows',
 # 워드 클라우드 표시
 # plt.imshow( ) : array에 색을 채워서 이미지로 표시
 plt.imshow(wordcloud)
+
+# 축 감추기
+plt.axis('off')
+plt.show()
