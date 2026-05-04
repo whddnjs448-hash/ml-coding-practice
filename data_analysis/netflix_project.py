4949361dd3006b9ed9b3a6f412118e5bb4cc3bd3
whddnjs448-hash <whddnjs448@gmail.com>
2026-05-04 21:49:50 +0900

    .

--- data_analysis/netflix_project.py
+++ data_analysis/netflix_project.py
@@ -46,3 +46,7 @@ netflix.info()
 # 데이터프레임의 각 컬럼별 결측치 개수 반환
 # isna() == isnull() : 결측 값은 True 반환하고, 그 외에는 False 반환
 netflix.isna().sum()
+
+# 시청 등급 기준표를 참고하여 Netflix의 rating 변수를 이용한 age_group_dic 변수 생성
+# rating 컬럼의 값을 age_group이라는 새로운 컬럼으로 복사
+netflix['age_group'] = netflix['rating']
