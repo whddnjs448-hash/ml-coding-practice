acf429777cb7b3a19a652bbed7e940419922eed4
whddnjs448-hash <whddnjs448@gmail.com>
2026-05-04 21:44:16 +0900

    .

--- data_analysis/netflix_project.py
+++ data_analysis/netflix_project.py
@@ -31,3 +31,5 @@ netflix['country'] = netflix['country'].fillna('No Data')
 
 # .replace(np.nan, 'b') : 결측치를 문자열 바꾸기 함수를 통해 처리
 # 결측치 비율 : director(29.91%), cast(9.37%)
+netflix['director'] = netflix['director'].replace(np.nan, 'No Data')
+netflix['cast'] = netflix['cast'].replace(np.nan, 'No Data')
