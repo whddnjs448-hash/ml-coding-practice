f73cf9276cbd0f63b442a28275ce0b5930f50ba4
whddnjs448-hash <whddnjs448@gmail.com>
2026-05-04 21:48:08 +0900

    .

--- data_analysis/netflix_project.py
+++ data_analysis/netflix_project.py
@@ -42,3 +42,7 @@ netflix.dropna(axis = 0, inplace=True)
 # .info() : 열에 대한 요약 정보 확인
 # 8807 rows(원본 데이터 행 개수) - 17 rows(결측치 행) = 8790 rows(결측치가 제거된 행 개수)
 netflix.info()
+
+# 데이터프레임의 각 컬럼별 결측치 개수 반환
+# isna() == isnull() : 결측 값은 True 반환하고, 그 외에는 False 반환
+netflix.isna().sum()
