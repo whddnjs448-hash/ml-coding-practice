be090b43a05095d1fc24a1d3da70f0819f68c3fc
whddnjs448-hash <whddnjs448@gmail.com>
2026-05-05 01:46:30 +0900

    .

--- data_analysis/netflix_project.py
+++ data_analysis/netflix_project.py
@@ -206,3 +206,6 @@ text = str(list(netflix['description']))
 # mask : 단어를 그릴 위치 설정, 흰색(#FFFFFF) 항목은 마스킹된 것으로 간주
 # 로고 이미지 열고 넘파이 배열로 변환
 mask = np.array(Image.open('netflix_logo.jpg'))
+
+# 워드 클라우드 색상맵 만들기
+cmap = plt.matplotlib.colors.LinearSegmentedColormap.from_list('', ['#221f1f','#b20710'])
