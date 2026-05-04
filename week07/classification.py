0b9445951e774042d7cfae2b73dbb3782f4290f1
whddnjs448-hash <whddnjs448@gmail.com>
2026-05-04 20:58:43 +0900

    Updated Changes

--- week07/classification.py
+++ week07/classification.py
@@ -3,11 +3,11 @@ from sklearn.datasets import fetch_openml
 
 mnist = fetch_openml('mnist_784', as_frame=False)
 
-print(mnist.key())   #data와 target만 사용
+print(mnist.keys())   #data와 target만 사용
 
 X, y = mnist.data, mnist.target 
 print(X)
-print(X.shape) #28 x 28개의 픽셀 특징을 가진 이미지 70,00개
+print(X.shape) #28 x 28개의 픽셀 특징을 가진 이미지 70,000개
 print(y)
 print(y.shape)
 
