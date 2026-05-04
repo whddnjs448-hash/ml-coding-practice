b4781af0ed59b604e95608354c3daecdc478569f
whddnjs448-hash <whddnjs448@gmail.com>
2026-05-05 00:45:21 +0900

    .

--- data_analysis/netflix_project.py
+++ data_analysis/netflix_project.py
@@ -163,3 +163,5 @@ netflix_age_country_unstack
 # 특정 나이 그룹에 따른 특정 나라별 콘텐츠로 필터링
 # 연령, 국가 리스트
 age_order = ['All', 'Older Kids', 'Teens', 'Adults']
+country_order = ['United States', 'India', 'United Kingdom', 'Canada', 'Japan',
+                 'France', 'South Korea', 'Spain', 'Mexico', 'Turkey']
