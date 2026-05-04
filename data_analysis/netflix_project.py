c0fc949c308f9a35f51e6050e349e5bb9fc5dcf6
whddnjs448-hash <whddnjs448@gmail.com>
2026-05-05 00:05:15 +0900

    .

--- data_analysis/netflix_project.py
+++ data_analysis/netflix_project.py
@@ -124,3 +124,8 @@ netflix['listed_in'].str.split(', ', expand=True)
 # [2단계] .stack( )을 사용하면 여러 열로 구성한 데이터프레임을 1개의 열로 만들어 쌓음
 # 예시) 인덱스 1의 listed_in 열 값 : International TV Shows, TV Dramas, TV Mysteries
 netflix['listed_in'].str.split(', ', expand=True).stack()
+
+# [3단계] .value_counts( )를 붙여 장르의 등장 횟수 계산
+# 예시) 인덱스 1의 listed_in 열 값 : International TV Shows, TV Dramas, TV Mysteries
+genres = netflix['listed_in'].str.split(', ', expand=True).stack().value_counts()
+genres
