4c1e7ac819e5ede8b71f8f126a87b9f3c4392a12
whddnjs448-hash <whddnjs448@gmail.com>
2026-05-05 01:44:27 +0900

    .

--- data_analysis/netflix_project.py
+++ data_analysis/netflix_project.py
@@ -195,3 +195,7 @@ plt.show()
 
 # WordCloud : 워드 클라우드 생성에 필요한 모듈
 # Image : 워드 클라우드를 원하는 형태로 그리기 위해 그림을 불러오는 패키지
+from wordcloud import WordCloud
+from PIL import Image
+
+plt.figure(figsize=(15, 5))
