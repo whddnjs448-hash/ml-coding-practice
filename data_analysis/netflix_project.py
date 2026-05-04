6008593688a7ba42f1b3b8362699e4ad20c4de28
whddnjs448-hash <whddnjs448@gmail.com>
2026-05-05 00:51:56 +0900

    .

--- data_analysis/netflix_project.py
+++ data_analysis/netflix_project.py
@@ -177,3 +177,8 @@ netflix_age_country_unstack
 # 나이 그룹에 따른 국가별 넷플릭스 콘텐츠 비율 구하기
 netflix_age_country_unstack = netflix_age_country_unstack.div(netflix_age_country_unstack.sum(axis=0), axis=1)
 netflix_age_country_unstack
+
+plt.figure(figsize=(15, 5))
+
+# 사용자 정의 컬러맵 만들기
+cmap = plt.matplotlib.colors.LinearSegmentedColormap.from_list('', ['#221f1f','#b20710','#f5f5f1'])
