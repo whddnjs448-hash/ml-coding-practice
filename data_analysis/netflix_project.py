6ec647c55fc569eb0c4f557428f0885f8c78f937
whddnjs448-hash <whddnjs448@gmail.com>
2026-05-05 01:24:57 +0900

    .

--- data_analysis/netflix_project.py
+++ data_analysis/netflix_project.py
@@ -190,3 +190,8 @@ plt.suptitle('Target ages proportion of total content by country',
 plt.title('Here we see interesting differences between countries. Most shows in South Korea are targeted to adults, for instance.',
           fontsize=12, fontfamily='serif')
 plt.show()
+
+# 넷플릭스 데이터의 description 열 이용한 워드 클라우드 생성
+
+# WordCloud : 워드 클라우드 생성에 필요한 모듈
+# Image : 워드 클라우드를 원하는 형태로 그리기 위해 그림을 불러오는 패키지
