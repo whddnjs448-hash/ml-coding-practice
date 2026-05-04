db7fa5d23bc86f9c09ecf1bc9dc2d022d1340000
whddnjs448-hash <whddnjs448@gmail.com>
2026-05-04 21:42:30 +0900

    .

--- data_analysis/netflix_project.py
+++ data_analysis/netflix_project.py
@@ -5,3 +5,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+
+# 세션 저장소에 업로드한 csv 파일을 읽어 변수에 할당
+netflix = pd.read_csv('netflix_titles.csv')
+netflix.head()
