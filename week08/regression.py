50f0e7beeb4ecd7fb94b23b6fdbe683a5e3155d1
whddnjs448-hash <whddnjs448@gmail.com>
2026-05-15 18:02:16 +0900

    .

--- week08/regression.py
+++ week08/regression.py
@@ -150,3 +150,10 @@ from sklearn.model_selection import train_test_split
 X = iris.data[["petal width (cm)"]].values
 y = iris.target_names[iris.target] == 'virginica'
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
+
+og_reg = LogisticRegression(random_state=42)
+log_reg.fit(X_train, y_train)
+
+print(log_reg.predict([[1.7], [1.5]]))
+
+# 소프트맥스 회귀
