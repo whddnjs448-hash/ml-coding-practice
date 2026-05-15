548e2ed1e0b74ec3ec6bc7525dc01a653c52daf2
whddnjs448-hash <whddnjs448@gmail.com>
2026-05-15 16:52:07 +0900

    .

--- week08/svm.py
+++ week08/svm.py
@@ -4,3 +4,5 @@
 import numpy as np
 from sklearn.datasets import load_iris
 from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import LinearSVC
