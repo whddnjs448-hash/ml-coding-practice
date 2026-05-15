f35cd730b2b01408af68f2bb91808f5a5a90f2b7
whddnjs448-hash <whddnjs448@gmail.com>
2026-05-15 18:05:59 +0900

    .

--- week08/regression.py
+++ week08/regression.py
@@ -161,3 +161,9 @@ print(log_reg.predict([[1.7], [1.5]]))
 X = iris.data[["petal length (cm)", "petal width (cm)"]].values
 y = iris["target"]
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
+
+softmax_reg = LogisticRegression(C=30, random_state=42)
+softmax_reg.fit(X_train, y_train)
+
+print(softmax_reg.predict([[5, 2]]))
+print(softmax_reg.predict_proba([[5, 2]]).round(2))
