c507a5c8870e61084d6b57d94a33c5274081c9cd
whddnjs448-hash <whddnjs448@gmail.com>
2026-05-15 17:05:28 +0900

    .

--- week08/svm.py
+++ week08/svm.py
@@ -46,3 +46,12 @@ poly_kernel_svm_clf.fit(X, y)
 rbf_kernel_svm_clf = make_pipeline(StandardScaler(),
                                    SVC(kernel="rbf", gamma=5, C=0.001))
 rbf_kernel_svm_clf.fit(X, y)
+
+# SVM 회귀
+
+from sklearn.svm import LinearSVR
+
+# 이 세 줄은 간단한 선형 데이터셋을 생성합니다.
+np.random.seed(42)
+X = 2 * np.random.rand(50, 1)
+y = 4 + 3 * X[:, 0] + np.random.randn(50)
