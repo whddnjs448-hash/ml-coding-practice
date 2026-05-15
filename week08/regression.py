e74a1d2ad19541770f4891b0e591a0e13e6855f1
whddnjs448-hash <whddnjs448@gmail.com>
2026-05-15 17:30:18 +0900

    .

--- week08/regression.py
+++ week08/regression.py
@@ -34,3 +34,8 @@ _new = np.array([[0], [2]])
 print(lin_reg.predict(X_new))
 
 # 확률적 경사 하강법
+
+from sklearn.linear_model import SGDRegressor
+
+sgd_reg = SGDRegressor(max_iter=1000, tol=1e-5, penalty=None, eta0=0.01,
+                       n_iter_no_change=100, random_state=42)
