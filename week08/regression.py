284478f22eb10adcd461618f5a8c7a9d0de7e59c
whddnjs448-hash <whddnjs448@gmail.com>
2026-05-15 17:59:33 +0900

    .

--- week08/regression.py
+++ week08/regression.py
@@ -141,3 +141,8 @@ list(iris)
 
 print(iris.data.head(3))
 print(iris.target.head(3))  # 샘플이 섞여 있지 않습니다.
+
+print(iris.target_names)
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
