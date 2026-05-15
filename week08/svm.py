51cae5def668627ef88688193354c95dc0ef02a6
whddnjs448-hash <whddnjs448@gmail.com>
2026-05-15 17:02:23 +0900

    .

--- week08/svm.py
+++ week08/svm.py
@@ -33,3 +33,11 @@ polynomial_svm_clf = make_pipeline(
     LinearSVC(C=10, max_iter=10_000, dual=True, random_state=42)
 )
 polynomial_svm_clf.fit(X, y)
+
+# 다항식 커널
+
+from sklearn.svm import SVC
+
+poly_kernel_svm_clf = make_pipeline(StandardScaler(),
+                                    SVC(kernel="poly", degree=3, coef0=1, C=5))
+poly_kernel_svm_clf.fit(X, y)
