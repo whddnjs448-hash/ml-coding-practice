6b7c7f1d0a56876108dedc31f9153a2d777732bd
whddnjs448-hash <whddnjs448@gmail.com>
2026-05-15 17:00:03 +0900

    .

--- week08/svm.py
+++ week08/svm.py
@@ -26,3 +26,10 @@ from sklearn.datasets import make_moons
 from sklearn.preprocessing import PolynomialFeatures
 
 X, y = make_moons(n_samples=100, noise=0.15, random_state=42)
+
+polynomial_svm_clf = make_pipeline(
+    PolynomialFeatures(degree=3),
+    StandardScaler(),
+    LinearSVC(C=10, max_iter=10_000, dual=True, random_state=42)
+)
+polynomial_svm_clf.fit(X, y)
