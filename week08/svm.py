0a73792786503f8d4dda927e6ba2256597a8d74a
whddnjs448-hash <whddnjs448@gmail.com>
2026-05-15 16:53:00 +0900

    .

--- week08/svm.py
+++ week08/svm.py
@@ -6,3 +6,7 @@ from sklearn.datasets import load_iris
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import LinearSVC
+
+iris = load_iris(as_frame=True)
+X = iris.data[["petal length (cm)", "petal width (cm)"]].values
+y = (iris.target == 2)  # Iris virginica
