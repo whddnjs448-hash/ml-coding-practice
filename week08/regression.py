1798fbd2fd7ae177e5b8e6478a41db2d3ca3d454
whddnjs448-hash <whddnjs448@gmail.com>
2026-05-15 17:18:08 +0900

    .

--- week08/regression.py
+++ week08/regression.py
@@ -3,3 +3,6 @@ import matplotlib.pyplot as plt
 
 plt.rc('font', size=14)
 plt.rc('axes', labelsize=14, titlesize=14)
+plt.rc('legend', fontsize=14)
+plt.rc('xtick', labelsize=10)
+plt.rc('ytick', labelsize=10)
