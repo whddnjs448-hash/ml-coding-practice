35cdb26ff54c940ce8bc7d1ad9426582294510fe
whddnjs448-hash <whddnjs448@gmail.com>
2026-05-15 17:55:24 +0900

    .

--- week08/regression.py
+++ week08/regression.py
@@ -118,3 +118,9 @@ ridge_reg.fit(X, y)
 print(ridge_reg.predict([[1.5]]))
 
 # 라쏘 회귀
+
+from sklearn.linear_model import Lasso
+
+lasso_reg = Lasso(alpha=0.1)
+lasso_reg.fit(X, y)
+print(lasso_reg.predict([[1.5]]))
