35d897948516536a6f7da46ddc5d0f5a3056319a
whddnjs448-hash <whddnjs448@gmail.com>
2026-05-15 17:51:21 +0900

    .

--- week08/regression.py
+++ week08/regression.py
@@ -104,3 +104,7 @@ from sklearn.linear_model import Ridge
 
 ridge_reg = Ridge(alpha=0.1, solver="cholesky")
 ridge_reg.fit(X, y)
+print(ridge_reg.predict([[1.5]]))
+
+sgd_reg = SGDRegressor(penalty="l2", alpha=0.1 / m, tol=None,
+                       max_iter=1000, eta0=0.01, random_state=42)
