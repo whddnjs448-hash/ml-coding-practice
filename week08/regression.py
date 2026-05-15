d08d53ae68b8a591aeb1909a2cbaa2d952312fc9
whddnjs448-hash <whddnjs448@gmail.com>
2026-05-15 17:47:36 +0900

    .

--- week08/regression.py
+++ week08/regression.py
@@ -93,3 +93,9 @@ X_new = np.linspace(0, 3, 100).reshape(100, 1)
 
 # 방금 생성한 데이터셋을 간단히 살펴보기
 plt.figure(figsize=(6, 4))
+plt.plot(X, y, ".")
+plt.xlabel("$x_1$")
+plt.ylabel("$y$  ", rotation=0)
+plt.axis([0, 3, 0, 3.5])
+plt.grid()
+plt.show()
