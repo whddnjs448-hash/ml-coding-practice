ff6edff9b8a1f7b6a7d66f5f502cc509bc08be1c
whddnjs448-hash <whddnjs448@gmail.com>
2026-05-15 18:04:08 +0900

    .

--- week08/regression.py
+++ week08/regression.py
@@ -157,3 +157,7 @@ log_reg.fit(X_train, y_train)
 print(log_reg.predict([[1.7], [1.5]]))
 
 # 소프트맥스 회귀
+
+X = iris.data[["petal length (cm)", "petal width (cm)"]].values
+y = iris["target"]
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
