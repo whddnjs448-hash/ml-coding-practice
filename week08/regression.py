8eaa534c195a6f19027941a6549a974bdf0929df
whddnjs448-hash <whddnjs448@gmail.com>
2026-05-15 17:56:24 +0900

    .

--- week08/regression.py
+++ week08/regression.py
@@ -124,3 +124,9 @@ from sklearn.linear_model import Lasso
 lasso_reg = Lasso(alpha=0.1)
 lasso_reg.fit(X, y)
 print(lasso_reg.predict([[1.5]]))
+
+# 엘라스틱넷
+
+from sklearn.linear_model import ElasticNet
+
+elastic_net = ElasticNet(alpha=0.1, l1_ratio=0.5)
