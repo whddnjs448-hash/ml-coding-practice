63b4a9d9f78b6deb7250773a575bf922c500ed9b
whddnjs448-hash <whddnjs448@gmail.com>
2026-05-15 16:54:02 +0900

    .

--- week08/svm.py
+++ week08/svm.py
@@ -10,3 +10,7 @@ from sklearn.svm import LinearSVC
 iris = load_iris(as_frame=True)
 X = iris.data[["petal length (cm)", "petal width (cm)"]].values
 y = (iris.target == 2)  # Iris virginica
+
+svm_clf = make_pipeline(StandardScaler(),
+                        LinearSVC(C=1, dual=True, random_state=42))
+svm_clf.fit(X, y)
