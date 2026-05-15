5e2d0df313200329a30e587e4ed53f1319e7189f
whddnjs448-hash <whddnjs448@gmail.com>
2026-05-15 17:19:51 +0900

    .

--- week08/regression.py
+++ week08/regression.py
@@ -6,3 +6,8 @@ plt.rc('axes', labelsize=14, titlesize=14)
 plt.rc('legend', fontsize=14)
 plt.rc('xtick', labelsize=10)
 plt.rc('ytick', labelsize=10)
+
+import numpy as np
+
+np.random.seed(42)                        # 코드 예제를 재현 가능하게 만들기 위해
+m = 100                                   # 샘플 개수
