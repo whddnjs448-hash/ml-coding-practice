e5b2e0069f40e0caa01ba5d2ec0da8daad0bfa09
whddnjs448-hash <whddnjs448@gmail.com>
2026-05-15 17:41:14 +0900

    .

--- week08/regression.py
+++ week08/regression.py
@@ -70,3 +70,8 @@ lin_reg.fit(X_poly, y)
 lin_reg.intercept_, lin_reg.coef_
 
 X_new = np.linspace(-3, 3, 100).reshape(100, 1)
+X_new_poly = poly_features.transform(X_new)
+y_new = lin_reg.predict(X_new_poly)
+
+plt.figure(figsize=(6, 4))
+plt.plot(X, y, "b.")
