847756c5fe6b2032598e5a1e64a1fc00e74e901f
whddnjs448-hash <whddnjs448@gmail.com>
2026-05-15 17:45:49 +0900

    .

--- week08/regression.py
+++ week08/regression.py
@@ -88,3 +88,8 @@ plt.show()
 np.random.seed(42)
 m = 20
 X = 3 * np.random.rand(m, 1)
+y = 1 + 0.5 * X + np.random.randn(m, 1) / 1.5
+X_new = np.linspace(0, 3, 100).reshape(100, 1)
+
+# 방금 생성한 데이터셋을 간단히 살펴보기
+plt.figure(figsize=(6, 4))
