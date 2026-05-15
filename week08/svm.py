5e1a808b7d3c75a5d3f026f9b9b463d5d8fbdd1b
whddnjs448-hash <whddnjs448@gmail.com>
2026-05-15 16:56:41 +0900

    .

--- week08/svm.py
+++ week08/svm.py
@@ -14,3 +14,8 @@ y = (iris.target == 2)  # Iris virginica
 svm_clf = make_pipeline(StandardScaler(),
                         LinearSVC(C=1, dual=True, random_state=42))
 svm_clf.fit(X, y)
+
+X_new = [[5.5, 1.7], [5.0, 1.5]]
+
+print(svm_clf.predict(X_new))
+print(svm_clf.decision_function(X_new))
