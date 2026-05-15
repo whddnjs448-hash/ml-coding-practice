296630d815cf0d4bbdb340f7776f836afcc1ebd7
whddnjs448-hash <whddnjs448@gmail.com>
2026-05-15 17:38:05 +0900

    .

--- week08/regression.py
+++ week08/regression.py
@@ -62,3 +62,7 @@ from sklearn.preprocessing import PolynomialFeatures
 
 poly_features = PolynomialFeatures(degree=2, include_bias=False)
 X_poly = poly_features.fit_transform(X)
+print(X[0])
+print(X_poly[0])
+
+lin_reg = LinearRegression()
