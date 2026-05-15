919e03e3684f839170222a79d5d9972e1c80a8c0
whddnjs448-hash <whddnjs448@gmail.com>
2026-05-15 17:53:28 +0900

    .

--- week08/regression.py
+++ week08/regression.py
@@ -113,3 +113,8 @@ print(sgd_reg.predict([[1.5]]))
 
 # 확률적 평균 GD(solver="sag")를 사용하면
 # 이전과 거의 동일한 솔루션을 얻을 수 있음을 보여줍니다.
+ridge_reg = Ridge(alpha=0.1, solver="sag", random_state=42)
+ridge_reg.fit(X, y)
+print(ridge_reg.predict([[1.5]]))
+
+# 라쏘 회귀
