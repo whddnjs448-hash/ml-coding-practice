a5ac3ffa136a1140a2bc0c279b03c2731b19d364
whddnjs448-hash <whddnjs448@gmail.com>
2026-05-26 13:54:16 +0900

    .

--- week09/decision_tree.py
+++ week09/decision_tree.py
@@ -56,3 +56,7 @@ y_quad = X_quad ** 2 + 0.025 * np.random.randn(200, 1)
 
 tree_reg = DecisionTreeRegressor(max_depth=2, random_state=42)
 tree_reg.fit(X_quad, y_quad)
+
+export_graphviz(
+    tree_reg,
+    out_file="regression_tree.dot",
