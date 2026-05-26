9e57d92af6a16a7677e4eaa75809e86110913f87
whddnjs448-hash <whddnjs448@gmail.com>
2026-05-26 14:21:09 +0900

    .

--- week09/decision_tree.py
+++ week09/decision_tree.py
@@ -103,3 +103,6 @@ th1, th2 = tree_clf_pca.tree_.threshold[[0, 2]]
 plt.plot([th1, th1], axes[2:], "k-", linewidth=2)
 plt.plot([th2, th2], axes[2:], "k--", linewidth=2)
 plt.text(th1 - 0.01, axes[2] + 0.05, "Depth=0",
+         horizontalalignment="right", fontsize=15)
+plt.text(th2 - 0.01, axes[2] + 0.05, "Depth=1",
+         horizontalalignment="right", fontsize=13)
