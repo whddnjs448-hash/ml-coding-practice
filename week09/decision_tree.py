fd0ded88ac906db061b4df734a184dbb4abc5516
whddnjs448-hash <whddnjs448@gmail.com>
2026-05-26 13:42:41 +0900

    .

--- week09/decision_tree.py
+++ week09/decision_tree.py
@@ -24,3 +24,8 @@ from graphviz import Source
 Source.from_file("iris_tree.dot")
 
 # 클래스 확률 추정
+
+print(tree_clf.predict_proba([[5, 1.5]]).round(3))
+print(tree_clf.predict([[5, 1.5]]))
+
+# 규제 매개변수
