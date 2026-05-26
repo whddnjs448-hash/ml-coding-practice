b669802eb4f44ee59d4b860eb914a730691853c6
whddnjs448-hash <whddnjs448@gmail.com>
2026-05-26 14:18:45 +0900

    .

--- week09/decision_tree.py
+++ week09/decision_tree.py
@@ -100,3 +100,6 @@ for idx, (name, style) in enumerate(zip(iris.target_names, ("yo", "bs", "g^"))):
 plt.xlabel("$z_1$")
 plt.ylabel("$z_2$", rotation=0)
 th1, th2 = tree_clf_pca.tree_.threshold[[0, 2]]
+plt.plot([th1, th1], axes[2:], "k-", linewidth=2)
+plt.plot([th2, th2], axes[2:], "k--", linewidth=2)
+plt.text(th1 - 0.01, axes[2] + 0.05, "Depth=0",
