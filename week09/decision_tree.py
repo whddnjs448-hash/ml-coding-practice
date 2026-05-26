5f8b1497e02dd504d17c0213d364149f72cf16b0
whddnjs448-hash <whddnjs448@gmail.com>
2026-05-26 13:47:18 +0900

    .

--- week09/decision_tree.py
+++ week09/decision_tree.py
@@ -38,3 +38,6 @@ tree_clf1 = DecisionTreeClassifier(random_state=42)
 tree_clf2 = DecisionTreeClassifier(min_samples_leaf=5, random_state=42)
 tree_clf1.fit(X_moons, y_moons)
 tree_clf2.fit(X_moons, y_moons)
+
+X_moons_test, y_moons_test = make_moons(n_samples=1000, noise=0.2,
+                                        random_state=43)
