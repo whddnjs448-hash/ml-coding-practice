1a4dc99693fdfdce9ea264d714c60bfb13647d1e
whddnjs448-hash <whddnjs448@gmail.com>
2026-05-26 13:34:55 +0900

    .

--- week09/decision_tree.py
+++ week09/decision_tree.py
@@ -4,3 +4,6 @@ from sklearn.tree import DecisionTreeClassifier
 iris = load_iris(as_frame=True)
 X_iris = iris.data[["petal length (cm)", "petal width (cm)"]].values
 y_iris = iris.target
+
+tree_clf = DecisionTreeClassifier(max_depth=2, random_state=42)
+tree_clf.fit(X_iris, y_iris)
