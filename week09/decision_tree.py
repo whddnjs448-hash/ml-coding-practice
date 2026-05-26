5408ab5091029d14e5c135e797fdadb627629ac8
whddnjs448-hash <whddnjs448@gmail.com>
2026-05-26 13:45:44 +0900

    .

--- week09/decision_tree.py
+++ week09/decision_tree.py
@@ -33,3 +33,8 @@ print(tree_clf.predict([[5, 1.5]]))
 from sklearn.datasets import make_moons
 
 X_moons, y_moons = make_moons(n_samples=150, noise=0.2, random_state=42)
+
+tree_clf1 = DecisionTreeClassifier(random_state=42)
+tree_clf2 = DecisionTreeClassifier(min_samples_leaf=5, random_state=42)
+tree_clf1.fit(X_moons, y_moons)
+tree_clf2.fit(X_moons, y_moons)
