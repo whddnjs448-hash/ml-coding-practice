c10cf227dc4923de97b8323409f490fd131834bf
whddnjs448-hash <whddnjs448@gmail.com>
2026-05-26 13:38:04 +0900

    .

--- week09/decision_tree.py
+++ week09/decision_tree.py
@@ -12,3 +12,6 @@ from sklearn.tree import export_graphviz
 
 export_graphviz(
         tree_clf,
+        out_file="iris_tree.dot",
+        feature_names=["petal length (cm)", "petal width (cm)"],
+        class_names=iris.target_names,
