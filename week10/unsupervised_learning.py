15185aa68c1e9c7bf92a1fe1f9f799d8a62ac872
whddnjs448-hash <whddnjs448@gmail.com>
2026-06-02 10:05:29 +0900

    .

--- week10/unsupervised_learning.py
+++ week10/unsupervised_learning.py
@@ -67,3 +67,7 @@ from sklearn.metrics import silhouette_score
 print(silhouette_score(X, kmeans.labels_))
 
 silhouette_scores = [silhouette_score(X, model.labels_) for model in kmeans_per_k[1:]]
+
+plt.figure(figsize=(8, 3))
+plt.plot(range(2, 10), silhouette_scores, "bo-")
+plt.xlabel("$k$")
