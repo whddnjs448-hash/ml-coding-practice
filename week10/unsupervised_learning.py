398efff3be2c9f0c1393117e7da7136a35342396
whddnjs448-hash <whddnjs448@gmail.com>
2026-06-02 09:46:21 +0900

    .

--- week10/unsupervised_learning.py
+++ week10/unsupervised_learning.py
@@ -2,3 +2,8 @@
 import matplotlib.pyplot as plt
 
 plt.rc('font', family='Malgun Gothic')  # 윈도우: 맑은 고딕
+plt.rcParams['axes.unicode_minus'] = False  # 마이너스(-) 기호 깨짐 방지
+
+plt.rc('font', size=14)
+plt.rc('axes', labelsize=14, titlesize=14)
+plt.rc('legend', fontsize=14)
