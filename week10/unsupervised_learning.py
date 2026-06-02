3fc16a288eb86a8a74a7125e9ea670605af87b73
whddnjs448-hash <whddnjs448@gmail.com>
2026-06-02 09:58:35 +0900

    .

--- week10/unsupervised_learning.py
+++ week10/unsupervised_learning.py
@@ -51,3 +51,7 @@ print(kmeans.score(X))
 
 kmeans_per_k = [KMeans(n_clusters=k, n_init=10, random_state=42).fit(X) for k in range(1, 10)]
 inertias = [model.inertia_ for model in kmeans_per_k]
+
+plt.figure(figsize=(8, 3.5))
+plt.plot(range(1, 10), inertias, "bo-")
+plt.xlabel("$k$")
