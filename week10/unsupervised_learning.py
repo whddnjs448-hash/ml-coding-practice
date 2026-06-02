5ef7ecd4276f8f7693d17208fb59eccc3bb588f3
whddnjs448-hash <whddnjs448@gmail.com>
2026-06-02 11:18:04 +0900

    .

--- week10/unsupervised_learning.py
+++ week10/unsupervised_learning.py
@@ -154,3 +154,14 @@ def plot_dbscan(dbscan, X, size, show_xlabels=True, show_ylabels=True):
     plt.title(f"eps={dbscan.eps:.2f}, min_samples={dbscan.min_samples}")
     plt.grid()
     plt.gca().set_axisbelow(True)
+    
+dbscan2 = DBSCAN(eps=0.2)
+dbscan2.fit(X)
+
+plt.figure(figsize=(9, 3.2))
+
+plt.subplot(121)
+plot_dbscan(dbscan, X, size=100)
+
+plt.subplot(122)
+plot_dbscan(dbscan2, X, size=600, show_ylabels=False)
