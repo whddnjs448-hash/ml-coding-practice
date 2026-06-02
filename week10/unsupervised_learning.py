c0eed25425e5eb3e8cea4f073c6b366ee6af75da
whddnjs448-hash <whddnjs448@gmail.com>
2026-06-02 10:33:57 +0900

    .

--- week10/unsupervised_learning.py
+++ week10/unsupervised_learning.py
@@ -104,3 +104,7 @@ for k in (3, 4, 5, 6):
     plt.gca().yaxis.set_major_formatter(FixedFormatter(range(k)))
     if k in (3, 5):
         plt.ylabel("클러스터")
+        
+    if k in (5, 6):
+        plt.gca().set_xticks([-0.1, 0, 0.2, 0.4, 0.6, 0.8, 1])
+        plt.xlabel("실루엣 계수")
