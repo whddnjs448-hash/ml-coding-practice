95a792dd0de59ac88f1e3312edba34cc8d9fef91
whddnjs448-hash <whddnjs448@gmail.com>
2026-06-02 11:07:05 +0900

    .

--- week10/unsupervised_learning.py
+++ week10/unsupervised_learning.py
@@ -134,3 +134,7 @@ def plot_dbscan(dbscan, X, size, show_xlabels=True, show_ylabels=True):
     core_mask[dbscan.core_sample_indices_] = True
     anomalies_mask = dbscan.labels_ == -1
     non_core_mask = ~(core_mask | anomalies_mask)
+    
+    cores = dbscan.components_
+    anomalies = X[anomalies_mask]
+    non_cores = X[non_core_mask]
