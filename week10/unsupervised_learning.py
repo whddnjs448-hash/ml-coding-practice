1b28a4d43b6e33f18eebec433300ec2bf3c3a7d2
whddnjs448-hash <whddnjs448@gmail.com>
2026-06-02 11:03:45 +0900

    .

--- week10/unsupervised_learning.py
+++ week10/unsupervised_learning.py
@@ -128,3 +128,6 @@ print(dbscan.labels_[:10])
 
 print(dbscan.core_sample_indices_[:10])
 print(dbscan.components_)
+
+def plot_dbscan(dbscan, X, size, show_xlabels=True, show_ylabels=True):
+    core_mask = np.zeros_like(dbscan.labels_, dtype=bool)
