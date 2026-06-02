be9cf480070def9d3fb0562a1f00cce3ae1fbdd1
whddnjs448-hash <whddnjs448@gmail.com>
2026-06-02 11:25:56 +0900

    .

--- week10/unsupervised_learning.py
+++ week10/unsupervised_learning.py
@@ -176,3 +176,7 @@ dbscan.fit(X)
 
 knn = KNeighborsClassifier(n_neighbors=50)
 knn.fit(dbscan.components_, dbscan.labels_[dbscan.core_sample_indices_])
+
+X_new = np.array([[-0.5, 0], [0, 0.5], [1, -0.1], [2, 1]])
+print(knn.predict(X_new))
+print(knn.predict_proba(X_new))
