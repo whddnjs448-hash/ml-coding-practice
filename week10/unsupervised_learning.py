8d036514a39128a43cf73d2d2b9b3f040d307c31
whddnjs448-hash <whddnjs448@gmail.com>
2026-06-02 09:48:46 +0900

    .

--- week10/unsupervised_learning.py
+++ week10/unsupervised_learning.py
@@ -12,3 +12,8 @@ plt.rc('ytick', labelsize=10)
 
 # K-평균
 from sklearn.cluster import KMeans
+from sklearn.datasets import make_blobs
+import numpy as np
+
+blob_centers = np.array([[ 0.2,  2.3], [-1.5 ,  2.3], [-2.8,  2.8],
+                         [-2.8,  4.2], [-2.8,  1.0]])
