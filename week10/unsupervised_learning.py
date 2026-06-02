6d2cdb70f9aad8e7f3e3d77ca81ba0814f719c62
whddnjs448-hash <whddnjs448@gmail.com>
2026-06-02 09:50:46 +0900

    .

--- week10/unsupervised_learning.py
+++ week10/unsupervised_learning.py
@@ -20,3 +20,7 @@ blob_centers = np.array([[ 0.2,  2.3], [-1.5 ,  2.3], [-2.8,  2.8],
 blob_std = np.array([0.4, 0.3, 0.1, 0.1, 0.1])
 X, y = make_blobs(n_samples=2000, centers=blob_centers, cluster_std=blob_std,
                   random_state=7)
+
+k = 5
+kmeans = KMeans(n_clusters=k, n_init=10, random_state=42)
+y_pred = kmeans.fit_predict(X)
