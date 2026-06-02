e7dc78a14ba64c055fbaccae6b3e55e26f397f67
whddnjs448-hash <whddnjs448@gmail.com>
2026-06-02 10:11:55 +0900

    .

--- week10/unsupervised_learning.py
+++ week10/unsupervised_learning.py
@@ -80,3 +80,8 @@ from sklearn.metrics import silhouette_samples
 from matplotlib.ticker import FixedLocator, FixedFormatter
 
 plt.figure(figsize=(11, 9))
+
+for k in (3, 4, 5, 6):
+    plt.subplot(2, 2, k - 2)
+
+    y_pred = kmeans_per_k[k - 1].labels_
