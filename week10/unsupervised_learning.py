2852da4b7896edfeff8b62225795f97614253965
whddnjs448-hash <whddnjs448@gmail.com>
2026-06-02 10:54:25 +0900

    .

--- week10/unsupervised_learning.py
+++ week10/unsupervised_learning.py
@@ -108,4 +108,8 @@ for k in (3, 4, 5, 6):
     if k in (5, 6):
         plt.gca().set_xticks([-0.1, 0, 0.2, 0.4, 0.6, 0.8, 1])
         plt.xlabel("실루엣 계수")
-        
+    else:
+        plt.tick_params(labelbottom=False)
+
+    plt.axvline(x=silhouette_scores[k - 2], color="red", linestyle="--")
+    plt.title(f"$k={k}$")
