4cf6181fcba5f2a1357dfd371b983f9fb6842533
whddnjs448-hash <whddnjs448@gmail.com>
2026-06-02 10:14:26 +0900

    .

--- week10/unsupervised_learning.py
+++ week10/unsupervised_learning.py
@@ -85,3 +85,8 @@ for k in (3, 4, 5, 6):
     plt.subplot(2, 2, k - 2)
 
     y_pred = kmeans_per_k[k - 1].labels_
+    silhouette_coefficients = silhouette_samples(X, y_pred)
+
+    padding = len(X) // 30
+    pos = padding
+    ticks = []
