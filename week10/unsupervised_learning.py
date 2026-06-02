aa605253296fbaccf6129034cf1b5461d76347d3
whddnjs448-hash <whddnjs448@gmail.com>
2026-06-02 10:21:19 +0900

    .

--- week10/unsupervised_learning.py
+++ week10/unsupervised_learning.py
@@ -95,3 +95,7 @@ for k in (3, 4, 5, 6):
         coeffs.sort()
 
         color = plt.cm.Spectral(i / k)
+        plt.fill_betweenx(np.arange(pos, pos + len(coeffs)), 0, coeffs,
+                          facecolor=color, edgecolor=color, alpha=0.7)
+        ticks.append(pos + len(coeffs) // 2)
+        pos += len(coeffs) + padding
