d6999d804a49a4bcf4fdef6ac39165303ce70b2c
whddnjs448-hash <whddnjs448@gmail.com>
2026-06-02 11:23:41 +0900

    .

--- week10/unsupervised_learning.py
+++ week10/unsupervised_learning.py
@@ -171,3 +171,8 @@ plt.show()
 from sklearn.neighbors import KNeighborsClassifier
 
 X, y = make_moons(n_samples=1000, noise=0.05, random_state=42)
+dbscan = DBSCAN(eps=0.2, min_samples=5)
+dbscan.fit(X)
+
+knn = KNeighborsClassifier(n_neighbors=50)
+knn.fit(dbscan.components_, dbscan.labels_[dbscan.core_sample_indices_])
